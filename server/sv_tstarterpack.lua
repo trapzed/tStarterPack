20eb9691a29c3e23f3ce566dfcfae7516e13bb7e
trapzed <trapzedcsgo@gmail.com>
2022-02-15 16:52:14 +0100

    Fix "identifier"

--- server/sv_tstarterpack.lua
+++ server/sv_tstarterpack.lua
@@ -4,49 +4,70 @@ TriggerEvent(Config.Events["esx:getSharedObject"], function(obj) ESX = obj end)
 
 RegisterServerEvent('tStarterPack:checkIfNoStarterPack')
 AddEventHandler('tStarterPack:checkIfNoStarterPack', function()
-	local player = source 
-	local identifier = GetPlayerIdentifiers(player)[1]
-	MySQL.Async.fetchAll('SELECT starterpack FROM users WHERE identifier=@id', {['@id'] = identifier}, function(result)
+    local _src = source
+    if (_src == nil) then return end
+
+    local xPlayer = ESX.GetPlayerFromId(_src)
+    if (xPlayer == nil) then return end
+
+    MySQL.Async.fetchAll('SELECT starterpack FROM users WHERE identifier=@id', {['@id'] = xPlayer.identifier}, function(result)
+        if result[1] == nil then 
+			return 
+		end
 		if result[1].starterpack == "0" then
-			TriggerClientEvent('tStarterPack:openMenu', player)
+            TriggerClientEvent('tStarterPack:openMenu', _src)
         else
-            TriggerClientEvent(Config.Events["esx:showNotification"], player, "~r~Vous avez déjà selectionné un starterpack")
+            TriggerClientEvent(Config.Events["esx:showNotification"], _src, "~r~Vous avez déjà selectionné un starterpack")
         end
-	end)
+    end)
 end)
 
 RegisterServerEvent('tStarterPack:legalPack')
 AddEventHandler('tStarterPack:legalPack', function()
-	local player = source 
-	local identifier = GetPlayerIdentifiers(player)[1]
-	MySQL.Async.fetchAll('SELECT starterpack FROM users WHERE identifier=@id', {['@id'] = identifier}, function(result)
+
+	local _src = source
+    if (_src == nil) then return end
+
+    local xPlayer = ESX.GetPlayerFromId(_src)
+    if (xPlayer == nil) then return end
+
+	MySQL.Async.fetchAll('SELECT starterpack FROM users WHERE identifier=@id', {['@id'] = xPlayer.identifier}, function(result)
+
+	if result[1] == nil then return end
+
 	if result[1].starterpack == "0" then
-		local xPlayer = ESX.GetPlayerFromId(player)
 		xPlayer.addMoney(Config.LegalMoneyNumber)
 		xPlayer.addInventoryItem(Config.LegalItem1, 10)
 		xPlayer.addInventoryItem(Config.LegalItem2, 10)
 		MySQL.Async.execute(
 				'UPDATE users SET starterpack=@starterpack WHERE identifier=@id',
 				{ 
-					['@id'] 				  = identifier ,
+					['@id'] 				  = xPlayer.identifier ,
 					['@starterpack']          = "legal",
 				},
 				function ()
 		end)
-		TriggerClientEvent(Config.Events["esx:showNotification"], player, "Vous avez selectionné le pack ~g~Legal")
+		TriggerClientEvent(Config.Events["esx:showNotification"], _src, "Vous avez selectionné le pack ~g~Legal")
 	else
-		TriggerClientEvent(Config.Events["esx:showNotification"], player, "Vous avez déjà selectionné un starterpack")
+		TriggerClientEvent(Config.Events["esx:showNotification"], _src, "Vous avez déjà selectionné un starterpack")
 	end
 	end)
 end)
 
 RegisterServerEvent('tStarterPack:illegalPack')
 AddEventHandler('tStarterPack:illegalPack', function()
-	local player = source 
-	local identifier = GetPlayerIdentifiers(player)[1]
-	MySQL.Async.fetchAll('SELECT starterpack FROM users WHERE identifier=@id', {['@id'] = identifier}, function(result)
+
+	local _src = source
+    if (_src == nil) then return end
+
+    local xPlayer = ESX.GetPlayerFromId(_src)
+    if (xPlayer == nil) then return end
+
+	MySQL.Async.fetchAll('SELECT starterpack FROM users WHERE identifier=@id', {['@id'] = xPlayer.identifier}, function(result)
+
+	if result[1] == nil then return end
+
 	if result[1].starterpack == "0" then
-		local xPlayer = ESX.GetPlayerFromId(player)
 		--local blackMoney = xPlayer.getAccount(Config.ArgentSale)
 
 		xPlayer.addAccountMoney(Config.ArgentSale, Config.IllegalMoneyNumber)
@@ -59,35 +80,43 @@ AddEventHandler('tStarterPack:illegalPack', function()
 		MySQL.Async.execute(
 				'UPDATE users SET starterpack=@starterpack WHERE identifier=@id',
 				{ 
-					['@id'] 				  = identifier ,
+					['@id'] 				  = xPlayer.identifier ,
 					['@starterpack']          = "illegal",
 				},
 				function ()
 		end)
-		TriggerClientEvent(Config.Events["esx:showNotification"], player, "Vous avez selectionné le pack ~r~Illegal")
+		TriggerClientEvent(Config.Events["esx:showNotification"], _src, "Vous avez selectionné le pack ~r~Illegal")
 	else
-		TriggerClientEvent(Config.Events["esx:showNotification"], player, "Vous avez déjà selectionné un starterpack")
+		TriggerClientEvent(Config.Events["esx:showNotification"], _src, "Vous avez déjà selectionné un starterpack")
 	end
 	end)
 end)
 
 RegisterServerEvent('tStarterPack:aucunPack')
 AddEventHandler('tStarterPack:aucunPack', function()
-	local player = source 
-	local identifier = GetPlayerIdentifiers(player)[1]
-	MySQL.Async.fetchAll('SELECT starterpack FROM users WHERE identifier=@id', {['@id'] = identifier}, function(result)
+
+	local _src = source
+    if (_src == nil) then return end
+
+    local xPlayer = ESX.GetPlayerFromId(_src)
+    if (xPlayer == nil) then return end
+
+	MySQL.Async.fetchAll('SELECT starterpack FROM users WHERE identifier=@id', {['@id'] = xPlayer.identifier}, function(result)
+
+	if result[1] == nil then return end
+
 	if result[1].starterpack == "0" then
 		MySQL.Async.execute(
 				'UPDATE users SET starterpack=@starterpack WHERE identifier=@id',
 				{ 
-					['@id'] 				  = identifier ,
+					['@id'] 				  = xPlayer.identifier ,
 					['@starterpack']          = "aucun",
 				},
 				function ()
 		end)
-		TriggerClientEvent(Config.Events["esx:showNotification"], player, "Vous avez choisi de démarrer sans pack")
+		TriggerClientEvent(Config.Events["esx:showNotification"], _src, "Vous avez choisi de démarrer sans pack")
 	else
-		TriggerClientEvent(Config.Events["esx:showNotification"], player, "Vous avez déjà selectionné un starterpack")
+		TriggerClientEvent(Config.Events["esx:showNotification"], _src, "Vous avez déjà selectionné un starterpack")
 	end
 	end)
 end)
